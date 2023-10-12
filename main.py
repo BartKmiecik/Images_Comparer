d05a71f7dea1669cfc7f83dc0cdd351c0e093ad1
Bart <bartoszkmiecik@katanaus.com>
2023-10-12 08:46:48 +0200

    Going back with colors

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ counter_second = 0
 img, img2 = None, None
 
 def error(img1, img2):
-   img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
-   img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+   # img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
+   # img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
    diff = cv2.subtract(img1, img2)
-   # diff = abs(img1 - img2)
+   # diff = img1 - img2
    err = np.sum(diff**2)
    mse = err/(float(height*width))
    # msre = np.sqrt(mse)
